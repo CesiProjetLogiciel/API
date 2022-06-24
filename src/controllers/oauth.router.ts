d4b582d31e514ec611d6acb5b58098e24755be6d
Creadeyh <amicard99@gmail.com>
2022-06-24 11:17:16 +0200

    Refreshing token flow

--- src/controllers/oauth.router.ts
+++ src/controllers/oauth.router.ts
@@ -49,7 +49,7 @@ oauthRouter.post("/register", apiKeyBearerMiddleware, async (req: Request, res: 
         var token: TokenResponse = {
             user_id: user_id,
             access_token: generateAccessToken(user_id),
-            expires_in: 604800,
+            expires_in: 1800,
             refresh_token: generateRefreshToken(user_id),
         }
         if (user.user_type == "THIRD_PARTY_DEV") {
@@ -65,19 +65,50 @@ oauthRouter.post("/register", apiKeyBearerMiddleware, async (req: Request, res: 
 
 oauthRouter.post("/token", apiKeyBearerMiddleware, async (req: Request, res: Response) => {
     try {
-        const item: Authentication | Refresh = req.body;
-  
-        // TODO
-        //const newItem = await ItemService.create(item);
-        var user_id: number = 5;
-        var token: TokenResponse = {
-            user_id: user_id,
-            access_token: generateAccessToken(user_id),
-            expires_in: 604800,
-            refresh_token: generateRefreshToken(user_id),
+        if (req.body.grant_type == "client_credentials") {
+            var login_body: Authentication = req.body;
+
+            // TODO check email password
+            var user_id: number = 6;
+
+            var token: TokenResponse = {
+                user_id: user_id,
+                access_token: generateAccessToken(user_id),
+                expires_in: 1800,
+                refresh_token: generateRefreshToken(user_id)
+            };
+            return res.status(200).json(token);
         }
-  
-        res.status(200).json(token);
+        else if (req.body.grant_type == "refresh_token") {
+            var refresh_body: Refresh = req.body;
+            jwt.verify(refresh_body.refresh_token, process.env.TOKEN_SECRET as string, (err: any, payload: any) => {
+                if (!refresh_body.refresh_token) {
+                    err = "error";
+                }
+                if (err) {
+                    console.log(err);
+                    return res.status(401).send({result: "Unauthorized. Invalid token"});
+                }
+                
+                var jwt_payload: Jwt = payload;
+    
+                if (jwt_payload.exp > (Date.now()/1000) && jwt_payload.type == "refresh") {
+                    var token: TokenResponse = {
+                        user_id: jwt_payload.sub,
+                        access_token: generateAccessToken(jwt_payload.sub),
+                        expires_in: 1800,
+                        refresh_token: generateRefreshToken(jwt_payload.sub)
+                    };
+                    return res.status(200).json(token);
+                }
+                else {
+                    return res.status(401).send({result: "Unauthorized. Invalid token"});
+                }
+            });
+        }
+        else {
+            return res.status(401).send({result: "Unauthorized. Invalid token"});
+        }
     } catch (e: any) {
         res.status(500).send(e.message);
     }
